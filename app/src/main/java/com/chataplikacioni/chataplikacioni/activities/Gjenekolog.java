b4b05b296bfb71b72fb3a8f28239f44703a9162f
BurimHimaj <Buriim17@gmail.com>
2018-03-30 03:09:57 +0200

    Ketu eshte Iterimi i pare ku une perveq SQLite db,login ,register dhe welcome message kam shtuar te gjitha kategorite e mjekeve :Pediater,Psikiater,Gjenekolog,Kirurg,Neurolog dhe Kardiolog te dizajnuara si CardView dhe jane te klikueshme ku per secilen kategori te mjekut qe klikon te hapet emri i mjekut dhe opsioni : Kontakto.

---
+++ app/src/main/java/com/chataplikacioni/chataplikacioni/activities/Gjenekolog.java
@@ -0,0 +1,15 @@
+package com.chataplikacioni.chataplikacioni.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chataplikacioni.chataplikacioni.R;
+
+public class Gjenekolog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gjenekolog);
+    }
+}
--- app/src/main/java/com/chataplikacioni/chataplikacioni/activities/UsersActivity.java
+++ app/src/main/java/com/chataplikacioni/chataplikacioni/activities/UsersActivity.java
@@ -4,47 +4,54 @@ package com.chataplikacioni.chataplikacioni.activities;
  * Created by User on 3/27/2018.
  */
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatTextView;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import com.chataplikacioni.chataplikacioni.R;
-import com.chataplikacioni.chataplikacioni.model.User;
-import com.chataplikacioni.chataplikacioni.sql.DatabaseHelper;
 
-import java.util.ArrayList;
-import java.util.List;
+public class UsersActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView gjenekologId, neurologId, kirurgId, psikiaterId, pediaterId, kardiologId;
 
-
-public class UsersActivity extends AppCompatActivity {
-
-    private AppCompatActivity activity = UsersActivity.this;
-    private AppCompatTextView textViewName;
-    private List<User> welcome;
-    private DatabaseHelper databaseHelper;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
-        getSupportActionBar().setTitle("");
-        initViews();
-        initObjects();
+        getSupportActionBar().hide();
+        gjenekologId = (CardView) findViewById(R.id.gjenekologId);
+        neurologId = (CardView) findViewById(R.id.neurologId);
+        kirurgId = (CardView) findViewById(R.id.kirurgId);
+        psikiaterId = (CardView) findViewById(R.id.psikiaterId);
+        pediaterId = (CardView) findViewById(R.id.pediaterId);
+        kardiologId = (CardView) findViewById(R.id.kardiologId);
+
+        gjenekologId.setOnClickListener(this);
+        neurologId.setOnClickListener(this);
+        kirurgId.setOnClickListener(this);
+        psikiaterId.setOnClickListener(this);
+        pediaterId.setOnClickListener(this);
+        kardiologId.setOnClickListener(this);
+
 
     }
 
-    /**
-     * incialiimi views
-     */
-    private void initViews() {
-        textViewName = (AppCompatTextView) findViewById(R.id.textViewName);
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.gjenekologId: i = new Intent(this,Gjenekolog.class); startActivity(i); break;
+            case R.id.neurologId : i = new Intent(this,Neurolog.class); startActivity(i);break;
+            case R.id.kirurgId : i = new Intent(this,Kirurg.class); startActivity(i);break;
+            case R.id.psikiaterId : i = new Intent(this,Psikiater.class); startActivity(i);break;
+            case R.id.pediaterId : i = new Intent(this,Pediater.class); startActivity(i);break;
+            case R.id.kardiologId : i = new Intent(this,Kardiolog.class); startActivity(i);break;
 
-    }
+            default:break;
 
-    private void initObjects() {
-        welcome = new ArrayList<>();
-        databaseHelper = new DatabaseHelper(activity);
-
+        }
     }
 }
+
